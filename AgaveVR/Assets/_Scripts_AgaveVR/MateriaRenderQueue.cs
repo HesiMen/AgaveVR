b9f43bdb093fd41be2c11ba02ff1efc4a18dee6c
HesiMen <hesi.mendez@gmail.com>
2021-01-16 15:38:22 -0800

    Hole Visual is working

---
+++ AgaveVR/Assets/_Scripts_AgaveVR/MateriaRenderQueue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MateriaRenderQueue : MonoBehaviour
+{
+    public Renderer mask;
+    public Renderer hole;
+    public int maskRenderQueue= 2000;
+    private void OnValidate()
+    {
+        //maskRenderQueue = hole.material.renderQueue;
+        mask.material.renderQueue = maskRenderQueue;
+        //mask.material.SetBuffer();
+
+        Debug.Log(mask.material.renderQueue);
+        Debug.Log(hole.material.renderQueue);
+    }
+}
