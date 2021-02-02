450a2f54fa90faa5ecc14eacf1de434958b95335
JonMoral <jonmoralesdesign@gmail.com>
2021-02-01 21:55:56 -0800

    Add Day/Night Timeline

---
+++ AgaveVR/Assets/ArtAssets/Shaders/SkyBox.shader
@@ -0,0 +1,82 @@
+﻿Shader "Custom/SkyBox"
+{
+    Properties
+    {
+        _MainTex ("Day Texture", Cube) = "white" {}
+        _DayColorMult("Day Color Multiplier", Color) = (0,0,0,0)
+        _SubTex ("Night Texture", 2D) = "white" {}
+
+        _AlphaValue("Alpha", Range(0,1)) = 1
+        _DayNightLerp ("Day Night Value", Range(0,1)) = 0
+        _BlendAmnt ("Blend Amount", Float) = 0
+
+        _Color1 ("Color 1", Color) = (1,1,1,1)
+        _Color2 ("Color 2", Color) = (1,1,1,1)
+        _Color3 ("Color 3", Color) = (1,1,1,1)
+        _SmoothScalar("Smooth Scalar", Float) = 0
+    }
+    SubShader
+    {
+
+        Tags { "RenderType"="Transparent" }
+        LOD 100
+        Cull Front
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            sampler2D _SubTex;
+            float4 _SubTex_ST;
+            half _DayNightLerp;
+            fixed4 _DayColorMult;
+            half _AlphaValue;
+
+            fixed4 _Color1;
+            fixed4 _Color2;
+            fixed4 _Color3;
+            fixed _BlendAmnt;
+            float _SmoothScalar;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                //fixed4 col = lerp(tex2D(_MainTex, i.uv) * _DayColorMult, tex2D(_SubTex, i.uv), _DayNightLerp);
+                // Col2 = FFE400 col1 = 00F1FF
+                //fixed4 sunsetBlend = smoothstep(_Color1 * i.uv.y, _Color2 * -i.uv.y, _SmoothScalar);
+                //fixed4 col = i.uv.y > _DayNightLerp ? sunsetBlend * (1-i.uv.y * _BlendAmnt): sunsetBlend * (1- i.uv.y*_BlendAmnt);
+                fixed4 col = smoothstep(_Color1 * (i.uv.y * _BlendAmnt), _Color2 * (1-i.uv.y), _SmoothScalar);
+                col.a = _AlphaValue;
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ AgaveVR/Assets/DayNightCycleManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycleManager : MonoBehaviour
+{
+    [Header("Day Night")]
+    [SerializeField] private GameObject _dayNightSky;
+    [SerializeField, Range(0,1)] private float _dayNightAlpha;
+    [SerializeField, Range(0,1)] private float _dayNightVal;
+    [SerializeField] private Vector2 _skyViewLimits;
+    [SerializeField] private MeshRenderer _skyMaterial;
+    [SerializeField] private float _dayLength;
+    [SerializeField] private float _duskDawnTransitionRate;
+    [SerializeField] private float _fadeOutLimit;
+    private Transform _dayNightTransform;
+    private bool _sphereRotate;
+    private bool _sunsetTime;
+    private float _lerpState;
+    private int _swithcLerp;
+    [SerializeField] private bool _day;
+
+
+    [Header("Sunset Sunrise")]
+    [SerializeField] private GameObject _sunsetSunriseSky;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _swithcLerp = -1;
+        _skyMaterial = _dayNightSky.GetComponent<MeshRenderer>();
+        _dayNightTransform = _dayNightSky.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _skyMaterial.material.SetFloat("_AlphaValue", _dayNightAlpha);
+
+        DayTransition();
+        //NightTransition();
+        RotateSky();
+    }
+
+    public void AlphaCtrl()
+    {
+        if (_dayNightTransform.rotation.x >= 60 || _dayNightTransform.rotation.y >= 60 || _dayNightTransform.rotation.z >= 60 || _dayNightTransform.rotation.x <= -60 || _dayNightTransform.rotation.y <= -60 || _dayNightTransform.rotation.z <= -60)
+        {
+            _sunsetTime = true;
+            //_sphereRotate = false;
+        }
+    }
+
+    public void NightTransition()
+    {
+        if (!_day)
+        {
+            _sphereRotate = false;
+            _dayNightAlpha = Mathf.Lerp(0, 1, _lerpState);
+            _lerpState -= _duskDawnTransitionRate * Time.deltaTime;
+
+            //_skyMaterial.material.SetFloat("_SmoothScalar", _skyViewLimits.x);
+
+            if (_lerpState <= 0)
+            {
+                _lerpState = 0;
+                _dayNightTransform.eulerAngles = new Vector3(180, 0, 0);
+
+                _skyMaterial.material.SetFloat("_SmoothScalar", _skyViewLimits.x);
+                _duskDawnTransitionRate *= -1;
+            }
+
+            if (_lerpState >= 1f)
+                _lerpState = 1;
+        }
+    }
+
+    public void DayTransition()
+    {
+        if(_day)
+        {
+            _dayNightAlpha = Mathf.Lerp(0, 1, _lerpState);
+            _lerpState -= _duskDawnTransitionRate * Time.deltaTime;
+
+            if (_lerpState <= 0)
+            {
+                _lerpState = 0;
+                if (_dayNightTransform.eulerAngles.z != 90)
+                    _dayNightTransform.eulerAngles = new Vector3(0, Random.Range(0, 360), 90);
+                _skyMaterial.material.SetFloat("_SmoothScalar", _skyViewLimits.y);
+                _duskDawnTransitionRate *= -1;
+            }
+
+            if(_lerpState >= 1)
+            {
+                _lerpState = 1;
+                _sphereRotate = true;
+            }
+        }
+    }
+
+    public void RotateSky()
+    {
+        if (_dayNightTransform.eulerAngles.z > -90 && _sphereRotate == true)
+            _dayNightTransform.Rotate(0, 0, -_dayLength, Space.Self);
+    }
+}
