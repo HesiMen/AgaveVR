a4981e59fe27f8c047a9220a16bfa19275d1204d
HesiMen <hesi.mendez@gmail.com>
2021-02-05 12:44:58 -0800

    GrabPrep

---
+++ AgaveVR/Assets/_Scripts_AgaveVR/VR_Script/GrabButtonInteration.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+using UnityEngine.XR;
+
+
+[Serializable] public class GrabEvent : UnityEvent { }
+public class GrabButtonInteration : MonoBehaviour
+{
+
+    public InputActionProperty grabAction;
+
+
+    public GrabEvent GrabButtonPressed;
+    public GrabEvent GrabButtonRel;
+
+
+    private void Awake()
+    {
+        grabAction.action.performed += OnGrabAction;
+    }
+    private void OnDisable()
+    {
+        grabAction.action.performed -= OnGrabAction;
+    }
+    private void OnGrabAction(InputAction.CallbackContext context)
+    {
+
+
+        bool pressed = context.ReadValueAsButton();
+
+        if (pressed)
+        {
+            GrabButtonPressed.Invoke();
+        }
+        else
+        {
+            GrabButtonRel.Invoke();
+        }
+
+
+    }
+}
