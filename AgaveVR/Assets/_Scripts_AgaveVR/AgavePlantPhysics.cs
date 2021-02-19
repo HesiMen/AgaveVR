583568bf881eca987ed5b0c83301c72b4a22c2dd
HesiMen <hesi.mendez@gmail.com>
2021-02-19 15:32:55 -0800

    Testing Plant Pgyisics

---
+++ AgaveVR/Assets/_Scripts_AgaveVR/AgavePlantPhysics.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgavePlantPhysics : MonoBehaviour
+{
+    public Collider[] agaveColliders;
+
+
+    private void Start()
+    {
+      //  agaveColliders = GetComponentsInChildren<Collider>();
+
+  
+    }
+
+}
