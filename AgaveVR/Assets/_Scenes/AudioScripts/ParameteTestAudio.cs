827c5c019c736bc28738089ebee7f07b4664c117
HesiMen <hesi.mendez@gmail.com>
2021-03-10 20:35:39 -0800

    new turn around movement

---
+++ AgaveVR/Assets/_Scenes/AudioScripts/ParameteTestAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParameteTestAudio : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string mySound;
+    public FMOD.Studio.EventInstance mySoundInstance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySoundInstance = FMODUnity.RuntimeManager.CreateInstance(mySound);
+
+        
+    }
+
+
+
+   public void ChangemySoundNowOfCave(float vol)
+    {
+        //mySoundInstance.setParameterByName(rideLily, vol);
+    }
+}
---
+++ AgaveVR/Assets/_Scripts_AgaveVR/VR_Script/rigAgaveVR.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class rigAgaveVR : UnityEngine.XR.Interaction.Toolkit.XRRig
+{
+
+    public float smoothRotationSpeed= 1f;
+    public void RotateCamSmooth(float angleDegrees)
+    {
+        Vector3 rotation = new Vector3(0f, angleDegrees, 0f);
+        rig.transform.DORotate(rotation, smoothRotationSpeed);
+    }
+
+}
